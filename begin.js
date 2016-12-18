2aaef03a226780a6ed558a3ff36a69ae3e63c06b
Attonasi <attonasi@hotmail.com>
2016-12-18 14:50:34 -0800

    this doesn't really count

---
+++ begin.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// A function wrap
+
+//event handler function
+//
+//  Takes the submitted name from the form and assigns it to a variable
+//  stores the kids name in local storage
+//  Return;
+//
+//event listener
+//
